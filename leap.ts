d1b31c8bca1481c1de4a65a3c0df04fb85916182
danail2003 <ddobrev20@outlook.com>
2022-01-22 11:26:40 +0200

    isLeap problem is solved

--- leap.ts
+++ leap.ts
@@ -1,5 +1,17 @@
 export function isLeap(year: number): boolean {
-    let isLeap: boolean=false;
+    let isLeap: boolean = false;
+
+    if (year % 4 === 0) {
+        isLeap = true;
+    }
+
+    if (year % 100 === 0) {
+        isLeap = false;
+    }
+
+    if (year % 400 === 0) {
+        isLeap = true;
+    }
 
     return isLeap;
 }
